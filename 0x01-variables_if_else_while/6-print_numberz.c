3cb2545009fe4112f4f58e99fc81af2418c3b3bc
chizychimzy <chizychimzy@gmail.com>
2022-03-11 17:55:55 -0800

    prints all single digit numbers of base 10 without char type

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -8,12 +8,10 @@ int main(void)
 {
 	int num;
 
-	for (num = 0; num < 10; num++)
+	for (num = '0'; num <= '9'; num++)
 	{
-		printf("%i", num);
+		putchar(num);
 	}
 	putchar('\n');
 	return (0);
 }
-
-
